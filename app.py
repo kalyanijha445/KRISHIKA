c87f6a680d8cb3e720545ca01698eab99f0002c3
kalyanijha445 <166299113+kalyanijha445@users.noreply.github.com>
2026-02-11 23:55:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config['PROFILE_FOLDER'] = 'static/profiles'
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  
 
 # API Setup
-genai.configure(api_key="AIzaSyBFBw8MSciZ-7ygwjTqcHOJgYElRPZ1Lys") 
+genai.configure(api_key="AIzaSyDhSagikZ_YKy_TCOELH33rdFpkaAIokMI") 
 model = genai.GenerativeModel('gemini-2.0-flash')
 
 # Ensure folders exist
